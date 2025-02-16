802552647ae52064834e4d40aaf88affd38ec6ce
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2025-02-16 19:02:48 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
+        hostname: "cdn.sanity.io/files/aqxam74o/production",
       },
     ],
   },
