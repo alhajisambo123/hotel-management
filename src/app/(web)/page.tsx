989b51299cf157117d54dce4bc780d932c784fa5
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2025-04-20 01:21:50 +0000

    Update page.tsx

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,7 +1,6 @@
 import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
 import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
-import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import PageSearch from "@/components/PageSearch/PageSearch";
 import { getFeaturedRoom } from "@/libs/apis";
 
