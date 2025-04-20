38b79d4b9e73cf39a60d94ff69764a39a765f02f
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2025-04-20 00:56:12 +0000

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
         <Link href="/" className="font-black text-tertiary-dark">
-          Hotelzz
+          Agenda
         </Link>
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
@@ -71,7 +71,7 @@ const Header = () => {
           <Link href="/">Home</Link>
         </li>
         <li className="hover:-translate-y-2 duration-500 transition-all">
-          <Link href="/rooms">Rooms</Link>
+          <Link href="/rooms">Tutors</Link>
         </li>
         <li className="hover:-translate-y-2 duration-500 transition-all">
           <Link href="/">Contact</Link>
