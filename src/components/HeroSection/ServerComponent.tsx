feaa1a2c123c8148e3826b558ce4814745c21f34
AHMED HUSSAIN SAMBO <alhajimalamsambo442@gmail.com>
2025-04-20 00:58:25 +0000

    Update ServerComponent.tsx

--- src/components/HeroSection/ServerComponent.tsx
+++ src/components/HeroSection/ServerComponent.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const heading1 = (
   <>
-    <h1 className="font-heading mb-6">Explore Our Exquisite Hotel</h1>
+    <h1 className="font-heading mb-6">Unlock Your Learning Potential</h1>
     <p className="text-[#4a4a4a] dark:text-[#ffffffea] mb-12 max-w-lg">
-      Experience an Exquisite Hotel Immersed in Rich History and Timeless
-      Elegance.
+      Connect with experienced student tutors for academic support and personal
+      learning.
     </p>
-    <button className="btn-primary">Get Started</button>
+    <Link href="/rooms">
+      <button className="btn-primary">Get Started</button>
+    </Link>
   </>
 );
 
@@ -15,7 +18,7 @@ export const section2 = (
   <div className="md:grid hidden gap-8 grid-cols-1">
     <div className="rounded-2xl overflow-hidden h-48">
       <Image
-        src="/images/hero-1.jpeg"
+        src="/images/hero-12.jpeg"
         alt="hero-1"
         width={300}
         height={300}
@@ -35,8 +38,8 @@ export const section2 = (
       </div>
       <div className="rounded-2xl overflow-hidden">
         <Image
-          src="/images/hero-3.jpeg"
-          alt="hero-3"
+          src="/images/hero-4.jpeg"
+          alt="hero-4"
           width={300}
           height={300}
           className="img scale-animation"
