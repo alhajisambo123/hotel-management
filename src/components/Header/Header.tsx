6e8436869933f17d3aeffa5ab434a5689019b3de
AHMED HUSSAIN <alhajimalamsambo442@gmail.com>
2025-05-16 17:23:01 +0000

    Initial com38

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   // const { data: session } = useSession();
 
   return (
-    <header className="sticky top-0 z-50 py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between transition-all duration-300  dark:bg-black focus:outline-none placeholder:text-black dark:placeholder:text-white">
+    <header className="sticky top-0 z-50 py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between transition-all duration-300 bg-white dark:bg-black">
       <div className="flex items-center w-full md:2/3">
         <Link href="/" className="font-black text-tertiary-dark">
           Agenda
